176ac7eeed22032d531ac375d6265248f29f5b88
Yuki Sumida <y.sumida.0201@gmail.com>
2018-09-10 21:40:55 +0900

    UITableViewを配置

--- TwitterLikeHeaderSample/ViewController.swift
+++ TwitterLikeHeaderSample/ViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+            cell.textLabel?.text = "\(indexPath.row)"
+        return cell
     }
 }
 
